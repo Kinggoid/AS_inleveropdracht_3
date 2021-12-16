b2f6956ce10b332510895d31bfbd0a31110b2f14
Kinggoid <thijmespambox@gmail.com>
2021-12-16 10:35:47 +0100

    Begin of project stucture.
    Because:
    - We want a nice stucture to begin with.
    
    I added a main and an agent file.

---
+++ main/main.py
@@ -0,0 +1,9 @@
+import tensorflow as tensor
+
+import gym
+env = gym.make('LunarLander-v2')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+env.close()
