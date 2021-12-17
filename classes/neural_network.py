43b5ce54dfdca981a4ceba33b82f64fed7b131b2
Kinggoid <thijmespambox@gmail.com>
2021-12-17 10:58:16 +0100

    Beginning of train function.

--- classes/neural_network.py
+++ classes/neural_network.py
@@ -7,7 +7,7 @@ def target_neural_network(state):
 
 
 def train(memory):
-    """Train the neural networks."""
+    """Train the approximator neural networks."""
     gamma = 0.9
     batch = memory.sample()
     batch_next_states = [sars.next_state for sars in batch]
