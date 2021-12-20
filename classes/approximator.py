866d9b662fe990710bece0ad748a27d247f074db
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-12-20 11:58:14 +0100

    Approximator class eerste versie is klaar.

--- classes/approximator.py
+++ classes/approximator.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
-import keras.backend as k
 import numpy as np
 
 class BaseNetwork:
     """Base class which defines the network"""
     def __init__(self):
+        # Base network
         inputlayer = tf.keras.layers.Input(shape=(8,))
         hidden1 = tf.keras.layers.Dense(32)(inputlayer)
         hidden2 = tf.keras.layers.Dense(32)(hidden1)
@@ -12,19 +12,45 @@ class BaseNetwork:
         self.network = tf.keras.models.Model(inputs=inputlayer, outputs=outputlayer)
         self.network.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss="mse")
 
-    def set_weights_manual(self):
-        pass
+    def get_output(self, inputs):
+        """Gives the output of the approximator neural network based
+        on a single or a set of inputs."""
+        if isinstance(inputs[0],list):
+            arrayinputs = np.array(inputs).reshape(len(inputs),8)  # https://stackoverflow.com/questions/70362733/input-to-the-neural-network-using-an-array
+        else:
+            arrayinputs = np.array(inputs).reshape(1,8)
+        return self.network.predict(arrayinputs)
+
+    def save_network(self):
+        """Returns the current model"""
+        return self.network
+
+    def load_network(self, loadnetwork):
+        """Loads a given model; overwrites current with new model!"""
+        self.network = loadnetwork
+
+    def train_network(self, x, y):
+        """Trains the network on a given set of X-values (state) and Y-values (Yqt)"""
+        if isinstance(x[0],list):  # X-set prep
+            arrayinputs = np.array(x).reshape(len(x),8)
+        else:
+            arrayinputs = np.array(x).reshape(1,8)
+
+        if isinstance(y[0],list):  # Y-set prep
+            arrayinputs = np.array(y).reshape(len(y),8)
+        else:
+            arrayinputs = np.array(y).reshape(1,8)
+
+        self.network.fit(x, y)
+
+    def set_weights(self, wandb: list, layer: int):
+        """Adjusts the weights of the given layer."""
+        temp = np.array(wandb)
+        if temp.shape == self.network.layer[layer].shape:
+            self.network.layer[layer].set_weights(wandb)  # TODO: Test of dit werkt in context! (Komt later)
 
     def get_weights(self):
         weights = []
         for layer in range(1,4):
-            weights.append(self.network.layers[layer].get_weights())
+            weights.append(np.array(self.network.layers[layer].get_weights()))
         return weights
-
-base = BaseNetwork()
-x = np.random.random((100,8))
-y = np.random.random((100,4))
-base.network.fit(x,y)
-print(base.network.layers[1].get_weights())
-# Layer 0 is input, geen weights.
-# Query op layers 1 t/m 3.
