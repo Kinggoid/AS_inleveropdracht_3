b1fb7f45e2e640f7236803461a72cebfc427d6c1
Kinggoid <thijmespambox@gmail.com>
2021-12-22 16:03:37 +0100

    Sla states op in main, en neural_network.py ziet er mooier uit.

--- classes/neural_network.py
+++ classes/neural_network.py
@@ -3,23 +3,8 @@ import numpy as np
 import tensorflow as tensor
 
 
-
-def get_state_values(environment):
-    # x = float
-    # y = float
-    # x_vol = float
-    # y_vol = float
-    # angle = float
-    # angle_vol = float
-    # right_leg = bool: int
-    # left_leg = bool: int
-    return deepcopy(environment.env)
-
-
-# def get_values_neural_network(model, state):
-#     q_values = model.predict(state)
-#     return q_values
-# DEPRECATED: Hier is al functionaliteit voor vanuit approximator.py
+def fit(state, target):
+    """"""
 
 
 def train(targetmodel, policymodel, memory, batchsize, gamma, actions):
@@ -27,27 +12,26 @@ def train(targetmodel, policymodel, memory, batchsize, gamma, actions):
     actions = {'fire_right_engine': '→', 'fire_left_engine': '←', 'fire_main_engine': '↓', 'nothing': '0'}
 
     batch = memory.sample(batchsize)
-    batch_current_states = [sarsd.get_state() for sarsd in batch]
-    batch_actions = [sarsd.get_action() for sarsd in batch]
-    batch_next_states = [sarsd.get_next_state() for sarsd in batch]
-    batch_rewards = [sarsd.get_reward() for sarsd in batch]
-    batch_done = [sarsd.get_done() for sarsd in batch]
+    # batch_current_states = [sarsd.get_state() for sarsd in batch]
+    # batch_actions = [sarsd.get_action() for sarsd in batch]
+    # batch_next_states = [sarsd.get_next_state() for sarsd in batch]
+    # batch_rewards = [sarsd.get_reward() for sarsd in batch]
+    # batch_done = [sarsd.get_done() for sarsd in batch]
     for i in range(len(batch)):
-        state = batch_current_states[i]
-        action = batch_actions[i]
-        next_state = batch_next_states[i]
-        reward = batch_rewards[i]
-        done = batch_rewards[i]
-        if done:
-            target = reward
+        state = batch[i]
+        next_state = state.next_state
+        if state.done:
+            target = state.reward
         else:
-            qvaluepolicy = policymodel.get_output(next_state)
-            bestaction = np.argmax(qvaluepolicy)
-            qvaluetarget = targetmodel.get_output(next_state)
-            bestactionqvalue = qvaluetarget[bestaction]
-            target = reward + gamma * bestactionqvalue
+            next_state_policies = policymodel.get_output(next_state)
+            bestaction = np.argmax(next_state_policies)
+
+            next_state_targets = targetmodel.get_output(next_state)
+            bestactionqvalue = next_state_targets[bestaction]
+            target = state.reward + gamma * bestactionqvalue
+
         tensortarget = policymodel.get_output(state)  # Tensorflow: Vervang de index beste actie met de target van de qvalues van target nn(?)
-        tensortarget[action] = target
+        tensortarget[state.action] = target
         # Voer backpropagation uit
         # Tensorflow: Voorbeeld: Target = 0.5, A* = 2: output = [30,50,20,10], target = [30,50,0.5,10]
         policymodel.train_network(next_state, tensortarget)
--- main/main.py
+++ main/main.py
@@ -9,6 +9,9 @@ from classes.memory import Memory
 
 env = gym.make('LunarLander-v2')
 memory = Memory(1000)
+sample_size = 64
+learning_rate = 0.01
+
 for i_episode in range(5000):
     observation = env.reset()
     state_reward, done = 0, 0
@@ -22,6 +25,9 @@ for i_episode in range(5000):
         observation, reward, done, info = env.step(action)
 
         sarsd = SARSd(last_observation, action, reward, observation, last_done)
+        Memory.append_to_memory(memory, sarsd)
+
+
 
         if done:
             print("Episode finished after {} timesteps".format(t+1))
