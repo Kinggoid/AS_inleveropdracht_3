1acdf888ba055b992809861e41e934e93a130bdf
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-12-23 09:20:59 +0100

    Cleanup whitespaces

--- main/main.py
+++ main/main.py
@@ -27,10 +27,6 @@ for i_episode in range(5000):
         sarsd = SARSd(last_observation, action, reward, observation, last_done)
         Memory.append_to_memory(memory, sarsd)
 
-
-
-
-
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
