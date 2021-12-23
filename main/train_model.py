3a2aa691f00924646ba27996dd414db494b5274e
SwagLag <50711973+SwagLag@users.noreply.github.com>
2021-12-23 16:42:50 +0100

    train() verbeterd met batching: snelheid verhoogt van 10 naar 2 seconden per episode. Oude functie heet nu train_serial().

--- main/train_model.py
+++ main/train_model.py
@@ -5,24 +5,31 @@ from classes.neural_network import *
 from classes.approximator import Approximator
 from classes.memory import Memory
 
+from datetime import datetime
+
 env = gym.make('LunarLander-v2')
 memory = Memory(10000)
-sample_size = 64
+batch_size = 64
 learning_rate = 0.0005
 policy_object = EpsilonGreedyPolicy()
 gamma = 0.9
 policy_network = Approximator()
 target_network = Approximator()
-copy_steps = 4
+copy_episodes = 4
+save_episodes = 100
 tau = 0.001
 
 # LOGGING
 episode_reward_total = 0
+timestart = datetime.now()
 
 for i_episode in range(5000):
     observation = env.reset()
     state_reward, done = 0, 0
     episode_reward = 0
+    timeend = datetime.now()
+    print("Iter took {}".format(timeend - timestart))
+    timestart = datetime.now()
     for t in range(1000):
 
         last_observation = observation
@@ -33,11 +40,11 @@ for i_episode in range(5000):
 
         episode_reward += reward
 
-
         sarsd = SARSd(last_observation, action, reward, observation, last_done)
         Memory.append_to_memory(memory, sarsd)
 
         if done:
+            end = datetime.now()
             episode_reward_total += episode_reward
             print("Episode: {}".format(i_episode))
             print("Episode finished after {} timesteps".format(t+1))
@@ -45,11 +52,12 @@ for i_episode in range(5000):
             print("Average reward over all episodes: {}".format((episode_reward_total/(i_episode+1))))
             break
 
-    train(target_network, policy_network, memory, sample_size, gamma)
-    if i_episode % copy_steps == 0 and i_episode > 0:
+    train(target_network, policy_network, memory, batch_size, gamma)
+    if i_episode % copy_episodes == 0 and i_episode > 0:
         copy_model(target_network, policy_network, tau)
 
-target_network.save_network("../savedmodels/target_network/v1")
-policy_network.save_network("../savedmodels/policy_network/v1")
+    if i_episode % save_episodes == 0 and i_episode > 0:
+        target_network.save_network("../savedmodels/target_network/v1")
+        policy_network.save_network("../savedmodels/policy_network/v1")
 
 env.close()
